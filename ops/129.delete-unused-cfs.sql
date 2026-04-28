92dc1e6f8023f9f68b2c04b2cdfcfb2fa7cfc9cc
Seraphys <xshatterx@protonmail.com>
2026-04-28 12:45:45 -0500

    Delete Unused CFs

---
+++ ops/129.delete-unused-cfs.sql
@@ -0,0 +1,33 @@
+-- @operation: export
+-- @entity: batch
+-- @name: Delete Unused CFs
+-- @exportedAt: 2026-04-28T17:45:43.943Z
+-- @opIds: 8035, 8036, 8037, 8038, 8039, 8040, 8041
+
+-- --- BEGIN op 8035 ( delete custom_format "Anime Bluray Tier 1" )
+delete from "custom_formats" where "name" = 'Anime Bluray Tier 1';
+-- --- END op 8035
+
+-- --- BEGIN op 8036 ( delete custom_format "Anime Bluray Tier 2" )
+delete from "custom_formats" where "name" = 'Anime Bluray Tier 2';
+-- --- END op 8036
+
+-- --- BEGIN op 8037 ( delete custom_format "Anime Bluray Tier 3" )
+delete from "custom_formats" where "name" = 'Anime Bluray Tier 3';
+-- --- END op 8037
+
+-- --- BEGIN op 8038 ( delete custom_format "Anime Bluray Tier 4" )
+delete from "custom_formats" where "name" = 'Anime Bluray Tier 4';
+-- --- END op 8038
+
+-- --- BEGIN op 8039 ( delete custom_format "Anime Bluray Tier 5" )
+delete from "custom_formats" where "name" = 'Anime Bluray Tier 5';
+-- --- END op 8039
+
+-- --- BEGIN op 8040 ( delete custom_format "Anime WEB-DL Tier 1" )
+delete from "custom_formats" where "name" = 'Anime WEB-DL Tier 1';
+-- --- END op 8040
+
+-- --- BEGIN op 8041 ( delete custom_format "Anime WEB-DL Tier 2" )
+delete from "custom_formats" where "name" = 'Anime WEB-DL Tier 2';
+-- --- END op 8041
